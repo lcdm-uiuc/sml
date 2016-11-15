9afa677ac3d76c7cee05e0ab892a15869fb2c87c
Kc Ikegwu <ikegwukc@gmail.com>
2016-11-15 13:35:24 -0600

    Fixed Bugs For plotting Graphs

--- sml/python/actions/metrics/visualize.py
+++ sml/python/actions/metrics/visualize.py
@@ -1,16 +1,18 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-def handle_plots(plot_types, keywords, model, df, X_train, y_train, X_test, y_test):
+def handle_plots(plot_types, keywords, algoType, model, df, X_train, y_train, X_test, y_test):
+
+	print (plot_types)
 	for plot_type in plot_types:
 		if plot_type == 'lattice':
 			plot_lattice(df, keywords)
 		if plot_type == 'ROC':
-			plot_ROC(model, X_test, y_test)
+			plot_ROC(algoType, model, keywords, df, X_test, y_test)
 		if plot_type == 'learnCurves':
-			learnCurves(X_train, y_train)
+			learnCurves(model, X_train, y_train) 
 		if plot_type == 'validationCurves':
-			validationCurves(X_test, y_test)
+			validationCurves(model, X_test, y_test)
 
 
 def plot_lattice(df, keywords):
@@ -24,54 +26,72 @@ def plot_lattice(df, keywords):
 	for col1, i in enumerate(columns):
 		for col2, j in enumerate(columns):
 			if i == j:
-				sns.kdeplot(iris_class_data[iris_class_data.columns[col1]], ax=ax[col1][col2], shade=True, legend=False)
+				sns.kdeplot(df[df.columns[col1]], ax=ax[col1][col2], shade=True, legend=False)
 			else:
-				sns.kdeplot( iris_class_data[iris_class_data.columns[col1]], iris_class_data[iris_class_data.columns[col2]], ax=ax[col1][col2])
+				sns.kdeplot( df[df.columns[col1]], df[df.columns[col2]], ax=ax[col1][col2])
 
 			# Formatting
-			if j == 0:
-				ax[i,j].set_xticklabels([])
-				ax[i,j].set_ylabel(column_headers[i])
-				ax[i,j].set_xlabel('')
-			if i == len(columns)-1:
-				ax[i,j].set_xlabel(column_headers[j])
-			elif i == len(columns)-1:
-				ax[i,j].tick_params(axis='y', which='major', bottom='off')
-				ax[i,j].set_yticklabels([])
-				ax[i,j].set_xlabel(column_headers[j])
-				ax[i,j].set_ylabel('')
-			else:
-				ax[i,j].set_xticklabels([])
-				ax[i,j].set_xlabel('')
+			ax[i,j].set_xticklabels([])
+			ax[i,j].set_yticklabels([])
+			ax[i,j].set_ylabel('')
+			ax[i,j].set_xlabel('')
 
-				ax[i,j].set_yticklabels([])
-				ax[i,j].set_ylabel('')
+			
+			# TODO: if they specify headers set x and y labels
+			#if j == 0:
+			#	ax[i,j].set_xticklabels([])
+			#	ax[i,j].set_ylabel(column_headers[i])
+			#	ax[i,j].set_ylabel('')
+			#	ax[i,j].set_xlabel('')
+			#if i == len(columns)-1:
+			#	ax[i,j].set_xlabel('')
+			#	ax[i,j].set_xlabel(column_headers[j])
+			#elif i == len(columns)-1:
+			#	ax[i,j].tick_params(axis='y', which='major', bottom='off')
+			#	ax[i,j].set_yticklabels([])
+			#	ax[i,j].set_xlabel(column_headers[j])
+			#	ax[i,j].set_ylabel('')
+			#else:
+			#	ax[i,j].set_xticklabels([])
+			#	ax[i,j].set_xlabel('')
+			#	ax[i,j].set_yticklabels([])
+			#	ax[i,j].set_ylabel('')
 
 	# TODO: Add Option to Save Figure
 	plt.show()
 	plt.close()
 
 
-def plot_ROC(model, keywords, df, X_test, y_test):
-
+def plot_ROC(algoType, model, keywords, df, X_test, y_test):
+	from sklearn.preprocessing import label_binarize
+	from sklearn.metrics import roc_curve, auc
+	import sklearn.cross_validation as cv
+	import numpy as np
+	#print (df)
 	fpr = dict()
 	tpr = dict()
 	roc_auc = dict()
 
-	labels = df[keywords.labels()]
+
+	# Must Binarize Data
+	_classes = y_test.unique().tolist()
+	labels = label_binarize(y_test, classes=_classes)
 	n_classes = labels.shape[1]
 
-	predict_score = model.decision_function(X_test)
+	X_test_bin, _ = cv.train_test_split(np.c_[X_test.values], test_size=0) # Binarize data
+	y_test_bin, _ = cv.train_test_split(labels, test_size=0) # Binarize data
+
+	predict_score = model.decision_function(X_test_bin)
 
 	# Generates ROC Curve
 	for i in range(n_classes):
-		fpr[i], tpr[i], _ = roc_curve(y_test[:, i], predict_score[:, i])
+		fpr[i], tpr[i], _ = roc_curve(y_test_bin[:, 1], predict_score[:, i])
 		roc_auc[i] = auc(fpr[i], tpr[i])
 	
 	# Plotting ROC Curve
-
+	line_width = 3  # TO DO: User can change line_width
 	for i in range(n_classes):
-		plt.plot(fpr[i], tpr[i], lw=line_width, label='ROC curve of class of {0} (Area = {1:0.2f})'.format(iris_classes[i], roc_auc[i]))
+		plt.plot(fpr[i], tpr[i], lw=line_width, label='ROC curve of class of {0} (Area = {1:0.2f})'.format(_classes[i], roc_auc[i]))
 
 	# TODO: They can specify y limit, x limit, x label, y label legend (location)
 
@@ -90,6 +110,7 @@ def plot_ROC(model, keywords, df, X_test, y_test):
 
 def learnCurves(model, X_train, y_train):
 	from sklearn.learning_curve import learning_curve
+	import numpy as np
 
 	# Generating Learning Curve
 	train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train) 
@@ -126,11 +147,15 @@ def learnCurves(model, X_train, y_train):
 
 def validationCurves(model, X_test, y_test):
 	from sklearn.learning_curve import validation_curve
+	import numpy as np
 
 	param_range = np.arange(0, 5)
 
 	# Generating Validation Curve
-	v_train_scores, v_test_scores = validation_curve(estimator, X_test, y_test, param_name='normalize', param_range=param_range)
+	for params in model.get_params().keys():  # KI: Doesn't seem to have an effect on validation curve will look into further
+		_param = params
+		break;
+	v_train_scores, v_test_scores = validation_curve(model, X_test, y_test, param_name=params, param_range=param_range)
 
 	plt.figure()
 	plt.xlabel("Validation examples")
@@ -147,10 +172,10 @@ def validationCurves(model, X_test, y_test):
 	plt.fill_between(param_range, v_test_scores_mean - v_test_scores_std,
 	                 v_test_scores_mean + v_test_scores_std, alpha=0.1, color="purple")
 
-	plt.plot(param_range, train_scores_mean, 'o-', color="orange",
+	plt.plot(param_range, v_train_scores_mean, 'o-', color="orange",
 	         label="Training score")
 
-	plt.plot(param_range, test_scores_mean, 'o-', color="purple",
+	plt.plot(param_range, v_test_scores_mean, 'o-', color="purple",
 	         label="Cross-validation score")
 
 	plt.legend(loc="best")
