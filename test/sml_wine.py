ffb33100f21485457124de19293c74928b167468
Kc Ikegwu <ikegwukc@gmail.com>
2016-12-07 01:20:48 -0600

    Changed names from mlsql to sml

---
+++ test/sml_wine.py
@@ -0,0 +1,6 @@
+import sml
+from sml import execute
+
+query = 'READ "data/wine.csv" (separator = ";", header = 0) AND SPLIT (train = .8, test = 0.2) AND CLASSIFY (predictors = [1,2,3,4,5,6,7,8,9,10,11], label = 12, algorithm = knn)'
+
+execute(query)
